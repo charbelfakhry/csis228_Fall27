169b6b4bf979c5bbc91dea9dfc652ab9aff88909
cfakhry <132930773+cfakhry@users.noreply.github.com>
2026-09-08 14:12:11 +0300

    js example

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-
-const add = (n1, n2) => {
-    return n1 + n2;
+// Json object
+const student = {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    age: 21,
+    major: "Computer Science"
 }
 
-console.log("The sum of 6 and 8 is", add(6, 8));
+// console.log(student.firstName);
+// console.log(student.lastName)
+// console.log(student.major);
+// console.log(student.age);
 
+// regular js function
+function getStudentFullName(student){
+    //let conc = student.firstName+" - "+student.lastName;
+    return `${student.firstName} - ${student.lastName}`
+}
+
+//arrow function.
+const getStudentAge = (student) =>{
+    return student.age;
+}
+
+console.log(getStudentFullName(student), getStudentAge(student))
