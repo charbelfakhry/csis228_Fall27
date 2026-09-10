b7b39c4002d2c367496a945604428ecd5ab46956
cfakhry <132930773+cfakhry@users.noreply.github.com>
2026-09-10 14:51:29 +0300

    js training

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const student = {
 // regular js function
 function getStudentFullName(student){
     //let conc = student.firstName+" - "+student.lastName;
-    return `${student.firstName} - ${student.lastName}`
+    return `jhjdhdf ${student.firstName} - ${student.lastName}`
 }
 
 //arrow function.
@@ -23,4 +23,126 @@ const getStudentAge = (student) =>{
     return student.age;
 }
 
-console.log(getStudentFullName(student), getStudentAge(student))
+const multiply = (n1, n2) => {
+    return n1 * n2;
+}
+
+// deconstruct 
+const {firstName, lastName, age} = student;
+
+
+// object spread
+const updatedStudent = {
+    ...student,
+    age: 99
+}
+
+const updateStudent = (student, changes) =>{
+    return {
+        ...student,
+        ...changes
+    }
+}
+
+const updated = updateStudent(student, 
+    {major: "Software engineering", age: 11});
+
+
+var students = [
+    {
+        id:1,
+        firstName: "John",
+        lastName: "Doe",
+        age: 16
+    },
+    {
+        id:2,
+        firstName: "Sarah",
+        lastName: "Smith",
+        age: 24
+    },
+    {
+        id:3,
+        firstName: "Mike",
+        lastName: "Brown",
+        age: 25
+    }
+];
+
+
+// traditional loop
+for(let i = 0; i < students.length; i++)
+{
+    if(students[i].age < 18)
+    {
+        console.log(students[i], "You are a minor");
+    }else{
+        console.log(students[i]);
+    }
+}
+
+// enhanced loop
+for(let i in students)
+{
+    console.log(students[i]);
+}
+
+for(const student of students)
+{
+    console.log("enhanced loop ", student);
+}
+
+const addStudent = (student) =>{
+    students.push(student);
+}
+
+addStudent(
+    {
+        firstName: "std 1", 
+        lastName: "ln 1", 
+        age: 9
+    }
+    );
+
+const studentOver20 = students.filter(student => student.age > 20);
+console.log(studentOver20);
+
+const names = students.map(student => `${student.firstName} - ${student.lastName}`);
+console.log(names);
+
+// three equalities also for the type 
+const exists = students.some(student => student.id === 3);
+console.log(exists);
+
+const updateStudent1 = (students, id, changes) => {
+    const index = students.findIndex(student => student.id === id);
+
+    if(index === -1){
+        return "Student does not exist!";
+    }
+
+    students[index] = {
+        ...students[index],
+        ...changes
+    };
+
+    return students[index];
+}
+
+const changedStudent = updateStudent1(students, 300, {age: 1000});
+console.log(changedStudent);
+
+// delete student
+
+const deleteStudent = (students, id) =>{
+    const index = students.findIndex(student => student.id === id);
+    // error first
+    if(index === -1){
+        return `Studnet ${id} does not exist!`
+    }
+
+    students.splice(index, 1);
+
+    return true;
+}
+
