bb76c320c88281e0439864aa623e973af8cc08f9
cfakhry <132930773+cfakhry@users.noreply.github.com>
2026-09-15 14:29:36 +0300

    express intro

--- index.js
+++ index.js
@@ -1,148 +1,43 @@
-// Json object
-const student = {
-    id: 1,
-    firstName: "John",
-    lastName: "Doe",
-    age: 21,
-    major: "Computer Science"
-}
+const express = require("express");
+const cors = require("cors");
 
-// console.log(student.firstName);
-// console.log(student.lastName)
-// console.log(student.major);
-// console.log(student.age);
+const app = express();
 
-// regular js function
-function getStudentFullName(student){
-    //let conc = student.firstName+" - "+student.lastName;
-    return `jhjdhdf ${student.firstName} - ${student.lastName}`
-}
+app.use(cors());
 
-//arrow function.
-const getStudentAge = (student) =>{
-    return student.age;
-}
+app.use(express.json());
 
-const multiply = (n1, n2) => {
-    return n1 * n2;
-}
+app.get("/test", (req, res) => {
 
-// deconstruct 
-const {firstName, lastName, age} = student;
+    res.json({message: "hello world"})
+    
+});
 
+// callback a function inside a function
+app.get("/", (req, res) =>{
+    // body of the callback
 
-// object spread
-const updatedStudent = {
-    ...student,
-    age: 99
-}
+    res.json(dummyUsers());
+});
 
-const updateStudent = (student, changes) =>{
-    return {
-        ...student,
-        ...changes
-    }
-}
+const dummyUsers = () => {
+    const users = [];
 
-const updated = updateStudent(student, 
-    {major: "Software engineering", age: 11});
-
-
-var students = [
+    for(let i = 0; i < 10000; i++)
     {
-        id:1,
-        firstName: "John",
-        lastName: "Doe",
-        age: 16
-    },
-    {
-        id:2,
-        firstName: "Sarah",
-        lastName: "Smith",
-        age: 24
-    },
-    {
-        id:3,
-        firstName: "Mike",
-        lastName: "Brown",
-        age: 25
-    }
-];
-
-
-// traditional loop
-for(let i = 0; i < students.length; i++)
-{
-    if(students[i].age < 18)
-    {
-        console.log(students[i], "You are a minor");
-    }else{
-        console.log(students[i]);
-    }
-}
-
-// enhanced loop
-for(let i in students)
-{
-    console.log(students[i]);
-}
-
-for(const student of students)
-{
-    console.log("enhanced loop ", student);
-}
-
-const addStudent = (student) =>{
-    students.push(student);
-}
-
-addStudent(
-    {
-        firstName: "std 1", 
-        lastName: "ln 1", 
-        age: 9
-    }
-    );
-
-const studentOver20 = students.filter(student => student.age > 20);
-console.log(studentOver20);
-
-const names = students.map(student => `${student.firstName} - ${student.lastName}`);
-console.log(names);
-
-// three equalities also for the type 
-const exists = students.some(student => student.id === 3);
-console.log(exists);
-
-const updateStudent1 = (students, id, changes) => {
-    const index = students.findIndex(student => student.id === id);
-
-    if(index === -1){
-        return "Student does not exist!";
+        users.push({
+            id: i,
+            name: `name ${i}`
+        });
     }
 
-    students[index] = {
-        ...students[index],
-        ...changes
-    };
-
-    return students[index];
+    return users;
 }
 
-const changedStudent = updateStudent1(students, 300, {age: 1000});
-console.log(changedStudent);
+const PORT = 3000;
 
-// delete student
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+})
 
-const deleteStudent = (students, id) =>{
-    const index = students.findIndex(student => student.id === id);
-    // error first
-    if(index === -1){
-        return `Studnet ${id} does not exist!`
-    }
-
-    students.splice(index, 1);
-
-    return true;
-}
 
